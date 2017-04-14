ac8880f0f3e55ca1022013ccd4c779e1f22c7985
Ivan Sanchez <s4nchez@gmail.com>
2017-04-14 14:32:59 +0100

    Change build to create executable standalone jar

--- src/main/kotlin/org/reekwest/httpbin/HttpBinLauncher.kt
+++ src/main/kotlin/org/reekwest/httpbin/HttpBinLauncher.kt
@@ -1,11 +1,10 @@
 package org.reekwest.httpbin
 
-import org.reekwest.http.core.Request
-import org.reekwest.http.core.body.bodyString
-import org.reekwest.http.core.ok
 import org.reekwest.http.jetty.startJettyServer
 
 fun main(args: Array<String>) {
-    { _: Request -> ok().bodyString("Hello World") }.startJettyServer()
+    HttpBin().startJettyServer(resolvePort(args))
 }
+
+private fun resolvePort(args: Array<String>) = if (args.isNotEmpty()) args[0].toInt() else 8000
 
