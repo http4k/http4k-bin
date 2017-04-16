ffc448a976263a9ab57f6459096b9994efdddbc5
Ivan Sanchez <s4nchez@gmail.com>
2017-04-16 23:00:11 +0100

    Add /headers

--- src/main/kotlin/org/reekwest/httpbin/HttpBin.kt
+++ src/main/kotlin/org/reekwest/httpbin/HttpBin.kt
@@ -13,13 +13,18 @@ import org.reekwest.http.routing.by
 import org.reekwest.http.routing.routes
 
 fun HttpBin(): HttpHandler = routes(
-    Method.GET to "/ip" by { request: Request -> ok().json(request.ipResponse()) }
+    Method.GET to "/ip" by { request: Request -> ok().json(request.ipResponse()) },
+    Method.GET to "/headers" by { request: Request -> ok().json(request.headerResponse()) }
 )
 
-private fun Response.json(ipResponse: IpResponse): Response =
-    bodyString(jacksonObjectMapper().writeValueAsString(ipResponse))
-        .header("content-type", "application/json")
+private fun Request.headerResponse(): HeaderResponse = HeaderResponse(mapOf(*headers.toTypedArray()))
 
 private fun Request.ipResponse() = IpResponse(headerValues("x-forwarded-for").joinToString(", "))
 
 data class IpResponse(val origin: String)
+
+data class HeaderResponse(val headers: Map<String, String?>)
+
+private fun Response.json(value: Any): Response =
+    bodyString(jacksonObjectMapper().writeValueAsString(value))
+        .header("content-type", "application/json")
--- src/test/kotlin/contract/org/reekwest/httpbin/HttpBinContract.kt
+++ src/test/kotlin/contract/org/reekwest/httpbin/HttpBinContract.kt
@@ -3,12 +3,14 @@ package contract.org.reekwest.httpbin
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.containsSubstring
+import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 import org.reekwest.http.core.HttpHandler
 import org.reekwest.http.core.Response
 import org.reekwest.http.core.body.bodyString
 import org.reekwest.http.core.get
 import org.reekwest.http.core.header
+import org.reekwest.httpbin.HeaderResponse
 import org.reekwest.httpbin.IpResponse
 
 abstract class HttpBinContract {
@@ -21,6 +23,15 @@ abstract class HttpBinContract {
         val ipResponse = response.bodyObject()
         assertThat(ipResponse.origin, containsSubstring("1.2.3.4"))
     }
+
+    @Test
+    fun returns_header_list_as_case_insensitive_map() {
+        val response = httpBin(get("/headers").header("my-header", "my-value"))
+        val headerResponse = response.headersResponse()
+        assertThat(headerResponse.headers.entries.find { it.key.equals("My-Header", true) }?.value, equalTo("my-value"))
+    }
 }
 
 private fun Response.bodyObject(): IpResponse = jacksonObjectMapper().readValue(bodyString(), IpResponse::class.java)
+
+private fun Response.headersResponse(): HeaderResponse = jacksonObjectMapper().readValue(bodyString(), HeaderResponse::class.java)
