1ce2ba0817db053914780181a2d1f5531db5cad0
Ivan Sanchez <s4nchez@gmail.com>
2017-04-19 22:50:39 +0100

    Add basic auth support

--- src/main/kotlin/org/reekwest/httpbin/HttpBin.kt
+++ src/main/kotlin/org/reekwest/httpbin/HttpBin.kt
@@ -1,6 +1,7 @@
 package org.reekwest.httpbin
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.reekwest.http.basicauth.basicAuthProtected
 import org.reekwest.http.core.HttpHandler
 import org.reekwest.http.core.Method
 import org.reekwest.http.core.Request
@@ -10,12 +11,20 @@ import org.reekwest.http.core.header
 import org.reekwest.http.core.headerValues
 import org.reekwest.http.core.ok
 import org.reekwest.http.routing.by
+import org.reekwest.http.routing.path
 import org.reekwest.http.routing.routes
 
 fun HttpBin(): HttpHandler = routes(
     Method.GET to "/ip" by { request: Request -> ok().json(request.ipResponse()) },
-    Method.GET to "/headers" by { request: Request -> ok().json(request.headerResponse()) }
+    Method.GET to "/headers" by { request: Request -> ok().json(request.headerResponse()) },
+    Method.GET to "/basic-auth/{user}/{pass}" by { request: Request ->
+        val user = request.path("user").orEmpty()
+        val pass = request.path("pass").orEmpty()
+        protectedResource(user).basicAuthProtected("reekwest-httpbin", user, pass).invoke(request)
+    }
 )
+
+fun protectedResource(user: String): HttpHandler = { _Request -> ok().json(AuthorizationResponse(user)) }
 
 private fun Request.headerResponse(): HeaderResponse = HeaderResponse(mapOf(*headers.toTypedArray()))
 
@@ -24,6 +33,8 @@ private fun Request.ipResponse() = IpResponse(headerValues("x-forwarded-for").jo
 data class IpResponse(val origin: String)
 
 data class HeaderResponse(val headers: Map<String, String?>)
+
+data class AuthorizationResponse(val user: String, val authenticated: Boolean = true)
 
 private fun Response.json(value: Any): Response =
     bodyString(jacksonObjectMapper().writeValueAsString(value))
--- src/test/kotlin/contract/org/reekwest/httpbin/HttpBinContract.kt
+++ src/test/kotlin/contract/org/reekwest/httpbin/HttpBinContract.kt
@@ -5,11 +5,14 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.containsSubstring
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
+import org.reekwest.http.basicauth.basicAuth
 import org.reekwest.http.core.HttpHandler
 import org.reekwest.http.core.Response
+import org.reekwest.http.core.Status
 import org.reekwest.http.core.body.bodyString
 import org.reekwest.http.core.get
 import org.reekwest.http.core.header
+import org.reekwest.httpbin.AuthorizationResponse
 import org.reekwest.httpbin.HeaderResponse
 import org.reekwest.httpbin.IpResponse
 
@@ -30,8 +33,20 @@ abstract class HttpBinContract {
         val headerResponse = response.headersResponse()
         assertThat(headerResponse.headers.entries.find { it.key.equals("My-Header", true) }?.value, equalTo("my-value"))
     }
+
+    @Test
+    fun supports_basic_auth() {
+        val response = httpBin.basicAuth("user", "passwd")(get("/basic-auth/user/passwd"))
+        assertThat(response.status, equalTo(Status.OK))
+        assertThat(response.authorizationResponse(), equalTo(AuthorizationResponse("user")))
+    }
 }
 
-private fun Response.bodyObject(): IpResponse = jacksonObjectMapper().readValue(bodyString(), IpResponse::class.java)
+private val mapper = jacksonObjectMapper()
 
-private fun Response.headersResponse(): HeaderResponse = jacksonObjectMapper().readValue(bodyString(), HeaderResponse::class.java)
+private fun Response.bodyObject(): IpResponse = mapper.readValue(bodyString(), IpResponse::class.java)
+
+private fun Response.headersResponse(): HeaderResponse = mapper.readValue(bodyString(), HeaderResponse::class.java)
+
+private fun Response.authorizationResponse(): AuthorizationResponse = mapper.readValue(bodyString(), AuthorizationResponse::class.java)
+
