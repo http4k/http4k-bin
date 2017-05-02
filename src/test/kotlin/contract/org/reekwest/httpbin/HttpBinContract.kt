ed0704eccc219a85cd8ee471ed11e86b603c352d
Ivan Sanchez <s4nchez@gmail.com>
2017-05-03 00:45:55 +0100

    Start testing cookie support

--- src/test/kotlin/contract/org/reekwest/httpbin/HttpBinContract.kt
+++ src/test/kotlin/contract/org/reekwest/httpbin/HttpBinContract.kt
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.containsSubstring
 import com.natpryce.hamkrest.equalTo
+import org.junit.Ignore
 import org.junit.Test
 import org.reekwest.http.basicauth.BasicAuthClient
 import org.reekwest.http.core.HttpHandler
 import org.reekwest.http.core.Request.Companion.get
 import org.reekwest.http.core.Response
 import org.reekwest.http.core.Status
+import org.reekwest.http.core.cookie.ClientCookies
 import org.reekwest.http.core.header
 import org.reekwest.http.core.then
+import org.reekwest.http.filters.ClientFilters.FollowRedirects
 import org.reekwest.httpbin.AuthorizationResponse
 import org.reekwest.httpbin.HeaderResponse
 import org.reekwest.httpbin.IpResponse
@@ -40,6 +43,22 @@ abstract class HttpBinContract {
         assertThat(response.status, equalTo(Status.OK))
         assertThat(response.authorizationResponse(), equalTo(AuthorizationResponse("user")))
     }
+
+    @Test
+    @Ignore("not implemented in reekwest-httpbin yet")
+    fun supports_cookies() {
+        val client = ClientCookies().then(httpBin)
+        assertThat(client(get("/cookies")).cookieResponse(), equalTo(CookieResponse(mapOf())))
+    }
+
+    @Test
+    @Ignore("not implemented in reekwest-httpbin yet")
+    fun supports_setting_cookies() {
+        val client = FollowRedirects().then(ClientCookies()).then(httpBin)
+        val response = client(get("/cookies/set").query("foo", "bar"))
+        println("response.bodyString() = ${response.bodyString()}")
+        assertThat(response.cookieResponse(), equalTo(CookieResponse(mapOf("foo" to "bar"))))
+    }
 }
 
 private val mapper = jacksonObjectMapper()
@@ -50,3 +69,6 @@ private fun Response.headersResponse(): HeaderResponse = mapper.readValue(bodySt
 
 private fun Response.authorizationResponse(): AuthorizationResponse = mapper.readValue(bodyString(), AuthorizationResponse::class.java)
 
+private fun Response.cookieResponse(): CookieResponse = mapper.readValue(bodyString(), CookieResponse::class.java)
+
+data class CookieResponse(val cookies: Map<String, String>)
