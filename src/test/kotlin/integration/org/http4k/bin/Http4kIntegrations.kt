5ad2936d5b44b228b7be3fd2e6dcfd5ad581f014
Ivan Sanchez <s4nchez@gmail.com>
2017-05-31 22:44:03 +0100

    Add Http4kIntegrations testing

---
+++ src/test/kotlin/integration/org/http4k/bin/Http4kIntegrations.kt
@@ -0,0 +1,52 @@
+@file:Suppress("UNUSED_PARAMETER")
+
+package integration.org.http4k.bin
+
+import contract.org.http4k.bin.HttpBinContract
+import org.http4k.bin.HttpBin
+import org.http4k.client.ApacheClient
+import org.http4k.client.OkHttp
+import org.http4k.core.HttpHandler
+import org.http4k.core.Uri
+import org.http4k.core.then
+import org.http4k.filter.ClientFilters
+import org.http4k.filter.DebuggingFilters
+import org.http4k.server.Jetty
+import org.http4k.server.Netty
+import org.http4k.server.ServerConfig
+import org.http4k.server.asServer
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+
+@RunWith(Parameterized::class)
+class Http4kIntegrations(name: String, client: HttpHandler, serverPort: Int) : HttpBinContract() {
+    override val httpBin = DebuggingFilters.PrintRequestAndResponse()
+        .then(ClientFilters.SetHostFrom(Uri.of("http://localhost:$serverPort")))
+        .then(client)
+
+    companion object {
+        val clients = listOf(
+            ClientSpec("okHttp", OkHttp()),
+            ClientSpec("apache", ApacheClient()))
+
+        val servers = listOf(
+            ServerSpec("jetty", Jetty(8000), 8000),
+            ServerSpec("netty", Netty(8001), 8001)
+        )
+
+        @Parameterized.Parameters(name = "{0}")
+        @JvmStatic
+        fun data(): Collection<Array<Any>> {
+            servers.forEach { HttpBin().asServer(it.config).start() }
+            return clients
+                .flatMap { client -> servers.map { server -> client to server } }
+                .map { arrayOf("${it.first.name} -> ${it.second.name}", it.first.handler, it.second.port) }
+        }
+    }
+
+    data class ClientSpec(val name: String, val handler: HttpHandler)
+    data class ServerSpec(val name: String, val config: ServerConfig, val port: Int)
+
+}
+
